f09aba67982565c7ba43f447216a0a85f5d29e8c
Renata Diaz <diaz.renatam@gmail.com>
2019-04-16 17:07:40 -0400

    First attempt to put plant data through workflow.

--- R/load_portal_plants.R
+++ R/load_portal_plants.R
@@ -40,5 +40,71 @@ load_portal_plants <- function(download = TRUE) {
 
   portal_plants <- list(winter_wide, summer_wide)
 
+  rm_list = ls()
+  rm(list = rm_list[ which(rm_list != "portal_plants")])
+  rm(rm_list)
   return(portal_plants)
 }
+
+#' Process Portal plant data
+#'
+#' @param portal_plants output of `load_portal_plants`
+#'
+#' @return matrix ready for use
+#' @export
+#'
+process_portal_plants <- function(portal_plants){
+
+  #Experimental plot identification numbers refer to experimental treatment as follows:
+  #Controls (2, 4, 8, 11, 12, 14, 17, 22), Kangaroo rat removals (3, 6, 13, 15, 18, 19, 20, 21),
+  #and total rodent removals (5, 7, 10, 16, 23).
+
+  portal_plants[[1]] <- dplyr::mutate(portal_plants[[1]], season = 'winter')
+  portal_plants[[2]] <- dplyr::mutate(portal_plants[[2]], season = 'summer')
+
+
+  assign_treatment <- function(plot_number){
+    if(plot_number %in% c(2,4,8,11,12,14,17,22)) {
+      return("control")
+    } else if(plot_number %in% c(5,7,10,16,23)) {
+      return("removal")
+    } else if(plot_number %in% c(3, 6, 13, 15, 18, 19, 20, 21)) {
+      return('krat_removal')
+    }
+  }
+
+  sort_plants <- function(plants) {
+    plants$trmt = vapply(plants$plot, FUN = assign_treatment, FUN.VALUE = "control")
+    plants_sorted <- as.data.frame(plants) %>%
+      dplyr::select(-year, -plot, -trmt, -season)
+    for(i in 1:nrow(plants_sorted)) {
+      this_rad <- sort(plants_sorted[i, ])
+      this_rad <- this_rad[which(this_rad >= 1)]
+      this_rad <- c(this_rad, rep(NA, times = length(plants_sorted[i, ]) - length(this_rad)))
+      plants_sorted[i,] <- this_rad
+    }
+    colnames(plants_sorted) <- c(1:ncol(plants_sorted))
+
+    plants_sorted <- cbind(dplyr::select(plants, year, season, trmt, plot), plants_sorted)
+
+    return(plants_sorted)
+  }
+
+  portal_plants_sorted <- lapply(portal_plants, sort_plants)
+
+  portal_plants_sorted[[1]] <- dplyr::mutate(portal_plants_sorted[[1]],
+                                             '44' = NA,
+                                             '45' = NA,
+                                             '46' = NA,
+                                             '47' = NA)
+
+  portal_plants_done <- rbind(portal_plants_sorted[[1]],
+                              portal_plants_sorted[[2]])
+
+  portal_plants_metadata <- dplyr::select(portal_plants_done, year, plot, trmt, season)
+  portal_plants_abund <- dplyr::select(portal_plants_done, -year, -plot, -trmt, -season)
+
+  return(list(portal_plants_metadata, portal_plants_abund))
+
+}
+
